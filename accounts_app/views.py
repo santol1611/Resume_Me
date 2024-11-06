fd40d62ab308ecc264c0550022fe6a75b6f2d03c
Kitsaran(Tong) <tong24232423@gmail.com>
2024-11-06 16:53:01 +0700

    :sparkles: add register.html :sparkles:
    เพิ่มหน้า register.html แต่ยังไม่ได้เชื่อมต่อกับส่วนอื่นๆ

--- accounts_app/views.py
+++ accounts_app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "accounts_app/index.html")
+
+def register(request):
+    return render(request, "accounts_app/register.html")
